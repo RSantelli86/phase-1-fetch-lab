9f310fcfc80da26930d916fcb3d66eb7eada38c9
Rey <rsantelli@luc.edu>
2024-02-11 18:46:43 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Sends a fetch request to the provided URL
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json()) // Parses the response as JSON
+    .then(json => renderBooks(json)); // Passes the JSON object to renderBooks
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = book.name; // Assuming 'name' is the correct property; adjust if necessary
     main.appendChild(h2);
   });
 }
